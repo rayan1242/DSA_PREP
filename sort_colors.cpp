a0c5f2cc662ce3d5115f1bdec896ce5512bf8d6b
rayan1242 <rayyanmaindargi12@gmail.com>
2023-09-11 17:13:14 +0530

    11/09/2023

---
+++ sort_colors.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int i=0,j=nums.size()-1,k=0;
+        while(k<=j){
+             if(nums[k]==0){
+                swap(nums[k],nums[i]);
+                i++,k++;
+            } else if(nums[k]==2){
+                swap(nums[k],nums[j]);
+                j--;                
+            } else {
+                    k++;
+                }
+        }
+
+    }
+};
