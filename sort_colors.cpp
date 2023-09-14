ea0bcaa73fcb41a92b0d8aedd97cea346580620a
rayan1242 <rayyanmaindargi12@gmail.com>
2023-09-14 13:51:04 +0530

    14th SEP 2023

--- sort_colors.cpp
+++ sort_colors.cpp
@@ -1,43 +1,18 @@
-#include<bits/stdc++.h>
-using namespace std;
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int i=0,j=nums.size()-1,k=0;
+        while(k<=j){
+             if(nums[k]==0){
+                swap(nums[k],nums[i]);
+                i++,k++;
+            } else if(nums[k]==2){
+                swap(nums[k],nums[j]);
+                j--;                
+            } else {
+                    k++;
+                }
+        }
 
-long maxBeauty(string str, int k, char ch){
-	long l=0,r=0;
-	long ans = INT_MIN;
-	int count=0;
-
-	while(r<str.size()){
-		while(r<str.size() and str[r]==ch){
-			ans = max(ans,r-l+1);
-			r++;
-		}
-		while(r<str.size() and str[r]!=ch and count<k){
-			count++;
-			ans = max(ans,r-l+1);
-			r++;	
-		}
-		while( l<r and str[r]!=ch and count>=k){
-			if(k==0){
-				l=++r;
-				break;
-			}
-			count-=(str[l]!=ch);
-			l++;
-		}	
-	}
-
-}
-
-int main(){
-
-	int n,k;
-	cin>>n>>k;
-	string str;
-	cin>>str;
-
-	long x = maxBeauty(str,k,'a');
-	long y = maxBeauty(str,k,'b');
-	cout<<max(x,y);
-	return 0;
-
-}
+    }
+};
